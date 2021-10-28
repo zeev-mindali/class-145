924542acae033cc50f901c6fe372412724452e1b
zeev mindali <mindalizeev@gmail.com>
2021-10-28 15:18:28 +0300

    class 2 - targilim

---
+++ src/class2_2810_loops/Targil2_doWhile.java
@@ -0,0 +1,16 @@
+package class2_2810_loops;
+
+import java.util.Scanner;
+
+public class Targil2_doWhile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        boolean exp1, exp2, exp3, result;
+        do {
+            System.out.println("please enter a number");
+            number = input.nextInt();
+
+        } while (number < 100 || number > 999 || number % 7 != 0);
+    }
+}
