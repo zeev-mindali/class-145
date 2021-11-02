3049c68516b2081ce7cd084dcfa36b23bce7730d
zeev mindali <mindalizeev@gmail.com>
2021-11-02 07:32:07 +0200

    class 2 - targilim

--- src/class2_2810_loops/Targil2_doWhile.java
+++ src/class2_2810_loops/Targil2_doWhile.java
@@ -6,11 +6,12 @@ public class Targil2_doWhile {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int number;
+        long num = 75219852198795218L;
         boolean exp1, exp2, exp3, result;
         do {
             System.out.println("please enter a number (not zero)");
             number = input.nextInt();
-
+            //test github upload
             //} while (number < 100 || number > 999 || number % 7 != 0);
         } while (number / 10 != number % 10);
     }
