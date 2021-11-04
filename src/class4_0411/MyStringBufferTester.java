7a89f1a6c7f336108c29e6c71dc9fe7e82b0c099
zeev mindali <mindalizeev@gmail.com>
2021-11-04 14:30:00 +0200

    class 3 - arrays

---
+++ src/class4_0411/MyStringBufferTester.java
@@ -0,0 +1,18 @@
+package class4_0411;
+
+public class MyStringBufferTester {
+    public static void main(String[] args) {
+        String myStory = "";
+        myStory += "My name is zeev\n";
+        myStory += "I live in qiryat yam";
+        myStory += "I work in JohnBryce/Dronix/Amm Security";
+
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("My name is zeev\n");
+        sb.append("I live in qiryat yam\n");
+        sb.append("I work in JohnBryce/Dronix/Amm Security\n");
+
+        System.out.println(sb);
+    }
+}
--- src/class4_0411/Tester.java
+++ src/class4_0411/Tester.java
@@ -2,13 +2,12 @@ package class4_0411;
 
 public class Tester {
     public static void main(String[] args) {
-        User admin = new User();
+        //User admin = new User();
 
-        admin.setLogin(true);
+        //admin.setLogin(true);
 //   if (admin.login("anna","12345")){
 ////            System.out.println("Hello my master !!!!");
 ////        } else {
 ////                 System.out.println("Why who are you?!?!?");
     }
-}
 }
