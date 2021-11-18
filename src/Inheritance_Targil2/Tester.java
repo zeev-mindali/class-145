51f1e107457aec1e55329b59baff649767e79cd9
zeev mindali <mindalizeev@gmail.com>
2021-11-18 09:03:20 +0200

    inheritance - targil 2

--- src/Inheritance_Targil2/Tester.java
+++ src/Inheritance_Targil2/Tester.java
@@ -1,9 +1,25 @@
 package Inheritance_Targil2;
 
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
         final float programmerSalary = 17000f;
         final float secretarySalary = 7000f;
+
+        Scanner input = new Scanner(System.in);
+
+        String userName;
+        int userId;
+        float userSalary;
+
+        /*
+        userName = input.nextLine();
+        userId = input.nextInt();
+        userSalary = input.nextFloat();
+
+        Programmer talBait = new Programmer(userName,userId,userSalary);
+        */
 
         Programmer tomer = new Programmer("Tomer", 1002, 17000f);
         Programmer talKita = new Programmer("Tal Rozner", 1003, 12000f);
