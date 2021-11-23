8692de585bb1ef3fabbd883d352c25652741d262
zeev mindali <mindalizeev@gmail.com>
2021-11-23 14:44:43 +0200

    Collection

--- src/class7_2311_files/TargilKita.java
+++ src/class7_2311_files/TargilKita.java
@@ -8,16 +8,31 @@ import java.util.Scanner;
 public class TargilKita {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("kabooom.txt");
-        PrintWriter pw = new PrintWriter(file);
-        Scanner input = new Scanner(System.in);
+//        PrintWriter pw = new PrintWriter(file);
+//        Scanner input = new Scanner(System.in);
+//
+//
+//        System.out.println("Enter a number :");
+//        int highBound = input.nextInt();
+//
+//        for (int counter = 0; counter <= highBound; counter++) {
+//            pw.println(counter % 7 == 0 ? "Boom" : counter);
+//        }
+//        pw.close();
 
-        System.out.println("Enter a number :");
-        int highBound = input.nextInt();
 
-        for (int counter = 0; counter <= highBound; counter++) {
-            pw.println(counter % 7 == 0 ? "Boom" : counter);
+        int highBound = 100;
+        int boomCounter = 0;
+        Scanner fileInput = new Scanner(file);
+        for (int counter = 0; counter < highBound; counter++) {
+            String readLine = fileInput.nextLine();
+            //System.out.println(readLine);
+            if (readLine.equals("Boom")) {
+                boomCounter++;
+            }
         }
+        System.out.println("Total boom:" + boomCounter);
 
-        pw.close();
+
     }
 }
