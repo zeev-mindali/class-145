1f6753caeaa05009a00d622bfd8f7d8e27782527
zeev mindali <mindalizeev@gmail.com>
2021-11-25 10:06:50 +0200

    Targil Exam

--- src/exam/Exam_Flow.java
+++ src/exam/Exam_Flow.java
@@ -20,7 +20,40 @@ public class Exam_Flow {
         System.out.println("Array 2\n=======");
         print(array2);
 
-        //
+        //print the lowest number from 4 cells in the array
+        System.out.println();
+        System.out.println(findLowestNumber(array1, 4));
+        System.out.println(findLowestNumber(array2, 4));
+
+        //find all unique number in array (distinct) in both arrays
+
+        //print array with all distinct number as small as possible
+    }
+
+    private static int findLowestNumber(int[] arr, int size) {
+        int lowNumber = Integer.MAX_VALUE;
+        for (int counter = 0; counter < arr.length - size; counter++) {
+            //7,1,5,2
+            int checkNumber = 0;
+            for (int index = 0; index < size; index++) {
+                checkNumber += arr[counter + index];
+                checkNumber *= 10;
+            }
+            checkNumber /= 10;
+            if (checkNumber < lowNumber) {
+                lowNumber = checkNumber;
+            }
+            /*
+            int checkNumber = arr[counter]*10;          //70
+            checkNumber=(checkNumber+arr[counter+1])*10;//710
+            checkNumber=(checkNumber+arr[counter+2])*10;//7150
+            checkNumber=(checkNumber+arr[counter+3]);   //7152
+            if (checkNumber<lowNumber){
+                lowNumber=checkNumber;
+            }
+            */
+        }
+        return lowNumber;
     }
 
     private static int[] getArray(int array_size) {
