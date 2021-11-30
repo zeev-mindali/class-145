761a6016732df1c00919a3eb74aa1a4f43d5c074
zeev mindali <mindalizeev@gmail.com>
2021-11-30 14:36:45 +0200

    drone lab

--- src/main/java/facade/RepairLab.java
+++ src/main/java/facade/RepairLab.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class RepairLab {
     private Scanner input = new Scanner(System.in);
