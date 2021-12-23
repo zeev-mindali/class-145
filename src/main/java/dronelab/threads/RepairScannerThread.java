242f4a22f93118e60aa565f337a6a357ec053b19
zeev mindali <mindalizeev@gmail.com>
2021-12-23 11:42:33 +0200

    drone lab

--- src/main/java/dronelab/threads/RepairScannerThread.java
+++ src/main/java/dronelab/threads/RepairScannerThread.java
@@ -28,47 +28,52 @@ public class RepairScannerThread implements Runnable {
             //itreate on entire collection
             for (Repair item : repairList) {
                 //check if we sent a message
-                if (!item.isPoped()) {
-                    //check if the date of the readyon is before current date
-                    //       23/12/2021                23/12/2021
-                    if (item.getReadyOn().isBefore(LocalDate.now())) {
-                        //print banner
-                        System.out.println(ArtUtil.droneReady);
-                        //print item
-                        System.out.println(item);
-                        //set isPoped to true, since we print the item to the screen
-                        item.setPoped(true);
-                        //check if the item is VIP,if it's vip send 3 messages every 30 seconds
-                        if (item.isImportent()) {
-                            //thread !!!!!!!!!!!!!!!!!!!!!!
-                            //anonymous thread
-                            new Thread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    //repeat message 3 times , VIP_TIMES=3
-                                    for (int counter = 0; counter < VIP_TIMES; counter++) {
-                                        try {
-                                            //go to sleep 30 sec.
-                                            Thread.sleep(30*1000);
-                                            //print VIP in big
-                                            System.out.println(ArtUtil.VIP);
-                                            //print repair details (item)
-                                            System.out.println(item);
-                                        } catch (InterruptedException e) {
-                                            e.printStackTrace();
-                                        }
-                                    }
-                                }
-                            }).start();
-                        }
-                    }
+                if (item.isPoped()) {
+                    continue;
                 }
+                //check if the date of the readyon is before current date
+                //       23/12/2021                23/12/2021
+                if (item.getReadyOn().isAfter(LocalDate.now())){
+                    continue;
+                }
+
+                //print banner
+                System.out.println(ArtUtil.droneReady);
+                //print item
+                System.out.println(item);
+                //set isPoped to true, since we print the item to the screen
+                item.setPoped(true);
+                //check if the item is VIP,if it's vip send 3 messages every 30 seconds
+                if (item.isImportent()) {
+                    //thread !!!!!!!!!!!!!!!!!!!!!!
+                    //anonymous thread
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //repeat message 3 times , VIP_TIMES=3
+                            for (int counter = 0; counter < VIP_TIMES; counter++) {
+                                try {
+                                    //go to sleep 30 sec.
+                                    Thread.sleep(30 * 1000);
+                                    //print VIP in big
+                                    System.out.println(ArtUtil.VIP);
+                                    //print repair details (item)
+                                    System.out.println(item);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }).start();
+                }
+
+
             }
             //don't check endlessly, take a little sleep
             try {
                 Thread.sleep(15 * 60 * 1000); //15*24*60*60*1000
             } catch (InterruptedException e) {
-                isRunning=false;
+                isRunning = false;
             }
         }
     }
