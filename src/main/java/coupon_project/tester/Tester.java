1887fe9331241f5c71a1d0d8ed00eff3eac77bfb
zeev mindali <mindalizeev@gmail.com>
2021-12-30 13:20:32 +0200

    project

---
+++ src/main/java/coupon_project/tester/Tester.java
@@ -0,0 +1,26 @@
+package coupon_project.tester;
+
+public class Tester {
+    public static void main(String[] args) {
+        /*
+                admin
+                ======================
+                login failed/successes
+                adminFacade.createCompany() X 5
+                check all admin methods
+
+
+                Company
+                ======================
+                login failed/successes
+                companyFacade.createCoupon() X 5
+                check all company methods also fail...
+
+                customer
+                ======================
+                login failed/successes
+                customerFacade.purchaseCoupon() X 5
+                check all customer methods
+         */
+    }
+}
